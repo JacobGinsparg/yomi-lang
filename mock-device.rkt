17ddff4d690e07700e4c3e8be11b85c55b5a021d
Mitchell McLean <mitchell.mclean@bookbub.com>
2018-03-14 16:21:58 -0400

    Implement mock device
    The mock device simply keeps track of what commands get sent to it.
    Nothing actually gets performed, making it suitable for development
    on unsupported platforms.
    
    The command list is accessible through "received-inputs", which may
    be useful for testing purposes.

---
+++ mock-device.rkt
@@ -0,0 +1,52 @@
+; Mock virtual device for testing and unsupported platforms.
+
+#lang racket
+
+(provide setup
+         press
+         hold
+         release
+         teardown
+         received-inputs)
+
+
+; fired-inputs is a list of InputEvent that can be viewed for testing purposes.
+; Newer events are pushed to the end of the list.
+(define received-inputs '())
+(define is-set-up #f)
+
+; An Event is a (cons State [Listof Input])
+; Signifies that all of the Inputs are being held/released simultaneously
+(define make-event cons)
+(define event-state first)
+(define event-inputs rest)
+
+; A State is one of:
+; - 'hold
+; - 'release
+
+(define (setup)
+  (set! is-set-up #t))
+
+(define (press . inputs)
+  (error-if-uninitialized 'press)
+  (set! received-inputs (append received-inputs (list (make-event 'hold inputs)
+                                                (make-event 'release inputs)))))
+
+(define (hold . inputs)
+  (error-if-uninitialized 'hold)
+  (set! received-inputs (append received-inputs (list (make-event 'hold inputs)))))
+
+(define (release . inputs)
+  (error-if-uninitialized 'release)
+  (set! received-inputs (append received-inputs (list (make-event 'release inputs)))))
+
+(define (teardown)
+  (if is-set-up
+      (begin (set! received-inputs '())
+             (set! is-set-up #f))
+      (error-if-uninitialized 'teardown)))
+
+(define (error-if-uninitialized sym)
+  (when (not is-set-up)
+    (error sym "device not initialized; run setup")))
