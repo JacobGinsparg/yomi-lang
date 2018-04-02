48ee5f9301f72af53d1e72ada65a942e7b6b5b1e
Mitchell McLean <mitchell.mclean@bookbub.com>
2018-04-02 13:06:23 -0400

    Use angle brackets instead of parens for delays
    To use parens in identifiers, we'd have to modify the reader. We've
    looked into this a bit and it's too involved for such a small piece of
    behavior. Luckily, the reader does not treat < and > like other
    brackets, so we can just use those and they work in identifiers just
    fine.
    
    This commit includes code changes and updates to the design docs. We
    originally wanted to use > for cancels; I've updated it to use ~ but
    we should have a conversation about this later.

--- hacking/regex-testing.rkt
+++ hacking/regex-testing.rkt
@@ -1,12 +1,12 @@
 #lang racket
 
-(define blah (regexp "([a-z]\\.)?([1-9](?:\\([0-9]+\\))?)*([A-z]+(?:\\+[A-z]+)*(?:\\([0-9]+\\))?)"))
+(define blah (regexp "^([a-z]\\.)?((?:[1-9](?:<[0-9]+>)?)+)?([A-z]+(?:\\+[A-z]+)*(?:<[0-9]+>)?)$"))
 
 (define m0 "j.236K")
 (define m1 "623HP+HK")
-(define m2 "4(30)6HP")
+(define m2 "4<30>6HP")
 (define m3 "j.HK")
-(define m4 "4(30)6HP+HK(10)")
+(define m4 "4<30>6HP+HK<10>")
 
 (define match regexp-match)
 (define match* regexp-match*)
--- test/character-schema-test.rkt
+++ test/character-schema-test.rkt
@@ -13,35 +13,35 @@
 
 ; Syntax is totally subject to change! Not sure how to set the game right now.
 (define-character some-character some-game
-  (move |HP| . . . .)                  ; One button
-  (move |MP+HP| . . . .)               ; Many buttons
-  (move |2HP| . . . .)                 ; One direction, one button
-  (move |2HP+HK| . . . .)              ; One direction, many buttons
-  (move |236HP| . . . .)               ; Many directions, one button
-  (move |236HP+HK| . . . .)            ; Many directions, many buttons
-  (move |2HP+HK(30)| . . . .)          ; Held buttons
-  (move |4(30)6HP| . . . .)            ; Held direction (one)
-  (move |2(10)3(20)6HP| . . . .)       ; Held direction (many)
-  (move |2(10)3(20)6HP(30)| . . . .))  ; Held buttons and directions
+  (move HP . . . .)                  ; One button
+  (move MP+HP . . . .)               ; Many buttons
+  (move 2HP . . . .)                 ; One direction, one button
+  (move 2HP+HK . . . .)              ; One direction, many buttons
+  (move 236HP . . . .)               ; Many directions, one button
+  (move 236HP+HK . . . .)            ; Many directions, many buttons
+  (move 2HP+HK<30> . . . .)          ; Held buttons
+  (move 4<30>6HP . . . .)            ; Held direction (one)
+  (move 2<10>3<20>6HP . . . .)       ; Held direction (many)
+  (move 2<10>3<20>6HP<30> . . . .))  ; Held buttons and directions
 (require (submod "." some-character))
 
 ; note that the last direction and the first button are performed separately
 (check-received-inputs (list (make-event 'hold (list HP))
                              (make-event 'release '(list HP)))
-                       |HP|)
+                       HP)
 (check-received-inputs (list (make-event 'hold (list MP HP))
                              (make-event 'release (list MP HP)))
-                       |MP+HP|)
+                       MP+HP)
 (check-received-inputs (list (make-event 'hold (list 'down))
                              (make-event 'release (list 'down))
                              (make-event 'hold (list HP))
                              (make-event 'release (list HP)))
-                       |2HP|)
+                       2HP)
 (check-received-inputs (list (make-event 'hold (list 'down))
                              (make-event 'release (list 'down))
                              (make-event 'hold (list HP HK))
                              (make-event 'release (list HP HK)))
-                       |2HP+HK|)
+                       2HP+HK)
 (check-received-inputs (list (make-event 'hold (list 'down))
                              (make-event 'release (list 'down))
                              (make-event 'hold (list 'down 'forward))
@@ -50,7 +50,7 @@
                              (make-event 'release (list 'forward))
                              (make-event 'hold (list HP))
                              (make-event 'release (list HP)))
-                       |236HP|)
+                       236HP)
 (check-received-inputs (list (make-event 'hold (list 'down))
                              (make-event 'release (list 'down))
                              (make-event 'hold (list 'down 'forward))
@@ -59,7 +59,7 @@
                              (make-event 'release (list 'forward))
                              (make-event 'hold (list HP HK))
                              (make-event 'release (list HP HK)))
-                       |236HP+HK|)
+                       236HP+HK)
 ; TODO
 ; - Hold tests (what's the best way to check for holds in received-inputs?)
 ; - Prefix tests
