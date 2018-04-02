244fd00f2608188113d1f3bef4e759ecf40c3213
Jacob Ginsparg <jacobginsparg@gmail.com>
2018-04-02 14:09:54 -0400

    [schema] Some move ideas

--- hacking/regex-testing.rkt
+++ hacking/regex-testing.rkt
@@ -1,5 +1,10 @@
 #lang racket
 
+(require (for-syntax syntax/parse
+                     syntax/to-string
+                     racket/syntax
+                     racket))
+
 (define blah (regexp "^([a-z]\\.)?((?:[1-9](?:<[0-9]+>)?)+)?([A-z]+(?:\\+[A-z]+)*(?:<[0-9]+>)?)$"))
 
 (define m0 "j.236K")
@@ -13,3 +18,14 @@
 
 (define (match-all regex . matches)
   (andmap (lambda (x) (regexp-match-exact? regex x)) matches))
+
+#;(define-syntax define-move
+  (syntax-parser
+    [(_ exprs ...)
+     (define the-string (first (match blah (syntax->string #'(exprs ...)))))
+     (define move-id (format-id #'(exprs ...) "~a" the-string #:source #'(exprs ...)))
+     (define new-id (datum->syntax #'(exprs ...) move-id))
+     (displayln new-id)
+     #`(define #,new-id #,the-string)]))
+
+#;(define-move 4(30)6HP+HK(10))
