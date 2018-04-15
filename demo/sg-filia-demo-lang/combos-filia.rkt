076dd4176dbb04007a5fbce2365e7081c8cd78d4
Mitchell McLean <m-mclean@users.noreply.github.com>
2018-04-15 18:54:01 -0400

    Complete Filia frame data; some new combos

--- demo/sg-filia-demo-lang/combos-filia.rkt
+++ demo/sg-filia-demo-lang/combos-filia.rkt
@@ -8,6 +8,17 @@
 
 ; Some combos! Finally!
 
-(define-combo basic-launcher 5LP ~ 5LP ~ 5LK ~ 5MP ~ 5HP)
+(define-combo basic-launcher LP ~ LP ~ LK ~ MP ~ HP)
 
-(define-combo basic-otg-fenrir 5HK & 2LP ~ 5MK ~ 5HP ~ fenrir)
+(define-combo basic-otg-fenrir HK & 2LP ~ MK ~ HP ~ fenrir)
+
+(define-combo foo
+  LP ~ LP ~ LK ~ MP ~ 2MK ~ HP
+  ~ 214MP+HP & 214MK+HK & 2LP ~ MK ~ HP ~ 623MP+HP)
+
+(define (go)
+  (setup)
+  (sleep 3)
+  (perform-combo foo)
+  (teardown))
+  
